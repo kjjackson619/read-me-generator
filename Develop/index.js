5c46adaacc0c19133adcbb967bd73a1fdb80f3a7
KJ Jackson <kjjackson619@yahoo.com>
2022-01-02 16:34:21 -0500

    inquirer install

--- Develop/index.js
+++ Develop/index.js
@@ -1,31 +1,15 @@
 const fs = require('fs');
+const generatePage = require('./template.js');
 
 const pageDataArgs = process.argv.slice(2, process.argv.length);
 
 const contributor = 'Kevin Jackson';
 
-const generatePage = (contributor) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>ReadMe Generator</title>
-    </head>
-  
-    <body>
-      <h1>ReadMe</h1>
-      <h2>Contribution By: ${contributor}</h2>
-    </body>
-    </html>
-    `;
-};
 
 
-fs.writeFile('index.html', generatePage(contributor), err => {
-    if (err) throw err;
+
+fs.writeFile('./index.html', generatePage(contributor), err => {
+    if (err) throw new Error(err);
 
     console.log('ReadMe complete! See index.html for output!');
 });
---
+++ Develop/template.js
@@ -0,0 +1,20 @@
+const generatePage = (contributor) => {
+  return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>ReadMe Generator</title>
+    </head>
+  
+    <body>
+      <h1>ReadMe</h1>
+      <h2>Contribution By: ${contributor}</h2>
+    </body>
+    </html>
+    `;
+};
+
+module.exports = generatePage;
